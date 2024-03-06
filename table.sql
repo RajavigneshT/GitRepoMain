07038992e136289ddd389d19287f21f0ef51bd79
Rajavignesh <rvthara96@gmail.com>
2024-03-06 07:40:53 +0530

    removed branch1 and main code

--- table.sql
+++ table.sql
@@ -1,15 +1,3 @@
-Table UM_USER {
-  user_id integer [primary key]
-  username varchar
-  role varchar
-  created_at timestamp
-}
-Table User2 {
-  user_id integer [primary key]
-  username varchar
-  role varchar
-  created_at timestamp
-}
 Table User3 {
   user_id integer [primary key]
   username varchar
